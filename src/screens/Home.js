7caa0bd7a63a805dfec31116cf3dbd5cb65367f6
Lakshya Gautam <gautamlakshya021@gmail.com>
2024-10-03 10:48:23 +0530

    first commit

---
+++ src/screens/Home.js
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from 'react'
+import Card from '../components/Card'
+// import Carousel from '../components/Carousel'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+export default function Home() {
+  const [foodCat, setFoodCat] = useState([])
+  const [foodItems, setFoodItems] = useState([])
+  const [search, setSearch] = useState('')
+  const loadFoodItems = async () => {
+    let response = await fetch("http://localhost:5000/api/auth/foodData", {
+      // credentials: 'include',
+      // Origin:"http://localhost:3000/login",
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+
+    });
+    response = await response.json()
+    // console.log(response[1][0].CategoryName)
+    setFoodItems(response[0])
+    setFoodCat(response[1])
+  }
+
+  useEffect(() => {
+    loadFoodItems()
+  }, [])
+
+  return (
+    <div >
+      <div>
+        <Navbar />
+      </div>
+      <div >
+
+        <div className='d-flex justify-content-center' style={{ height: "60vh", width: "100vw", position: "relative" }}>
+          <img src="/tandoori-paneer-tikka4.jpg" style={{ objectFit: "cover", filter: "brightness(50%)", height: "100%", width: "100%" }} alt="" />
+
+          <div style={{ zIndex: "9", position: "absolute", bottom: "10vh", width: "80%" }}>
+            <div className=" d-flex justify-content-center">  {/* justify-content-center, copy this <form> from navbar for search box */}
+              <input className="form-control me-2 w-75 bg-white text-dark" type="search" placeholder="Search in here..." aria-label="Search" value={search} onChange={(e) => { setSearch(e.target.value) }} />
+              <button className="btn text-white bg-danger" onClick={() => { setSearch('') }}>Clear</button>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <div className='container'> {/* boootstrap is mobile first */}
+        {
+          foodCat !== []
+            ? foodCat.map((data) => {
+              return (
+                // justify-content-center
+                <div className='row mb-3'>
+                  <div key={data.id} className='fs-3 m-3'>
+                    {data.CategoryName}
+                  </div>
+                  <hr id="hr-success" style={{ height: "4px", backgroundImage: "-webkit-linear-gradient(left,rgb(0, 255, 137),rgb(0, 0, 0))" }} />
+                  {foodItems !== [] ? foodItems.filter(
+                    (items) => (items.CategoryName === data.CategoryName) && (items.name.toLowerCase().includes(search.toLowerCase())))
+                    .map(filterItems => {
+                      return (
+                        <div key={filterItems.id} className='col-12 col-md-6 col-lg-3'>
+                          {console.log(filterItems.url)}
+                          <Card foodName={filterItems.name} item={filterItems} options={filterItems.options[0]} ImgSrc={filterItems.img} ></Card>
+                        </div>
+                      )
+                    }) : <div> No Such Data </div>}
+                </div>
+              )
+            })
+            : ""}
+      </div>
+      <Footer />
+    </div>
+
+
+
+
+
+
+
+
+
+  )
+}
