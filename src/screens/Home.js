f127d9d13301792534a16385f988586f6a13d60d
Lakshya Gautam <gautamlakshya021@gmail.com>
2024-10-03 10:57:40 +0530

    server updates

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -52,8 +52,8 @@ export default function Home() {
             ? foodCat.map((data) => {
               return (
                 // justify-content-center
-                <div className='row mb-3'>
-                  <div key={data.id} className='fs-3 m-3'>
+                <div key={data._id} className='row mb-3'>
+                  <div className='fs-3 m-3'>
                     {data.CategoryName}
                   </div>
                   <hr id="hr-success" style={{ height: "4px", backgroundImage: "-webkit-linear-gradient(left,rgb(0, 255, 137),rgb(0, 0, 0))" }} />
@@ -61,7 +61,7 @@ export default function Home() {
                     (items) => (items.CategoryName === data.CategoryName) && (items.name.toLowerCase().includes(search.toLowerCase())))
                     .map(filterItems => {
                       return (
-                        <div key={filterItems.id} className='col-12 col-md-6 col-lg-3'>
+                        <div key={filterItems._id} className='col-12 col-md-6 col-lg-3'>
                           {console.log(filterItems.url)}
                           <Card foodName={filterItems.name} item={filterItems} options={filterItems.options[0]} ImgSrc={filterItems.img} ></Card>
                         </div>
