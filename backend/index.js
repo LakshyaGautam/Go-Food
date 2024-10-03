50b9bfd2573cfa3826c270122330f18f2f59fb0a
Lakshya Gautam <gautamlakshya021@gmail.com>
2024-10-03 11:40:28 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,36 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
 
-global.foodData = require('./db')(function call(err, data, CatData) {
-  // console.log(data)
-  if(err) console.log(err);
-  global.foodData = data;
-  global.foodCategory = CatData;
-})
+global.foodData = [];
+global.foodCategory = [];
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
+require('./db')(function call(err, data, CatData) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    global.foodData = data;
+    global.foodCategory = CatData;
+    console.log('Data loaded successfully'); // Add a log to confirm data load
 });
-app.use(express.json())
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
 
 app.use('/api/auth', require('./Routes/Auth'));
 
 app.listen(port, () => {
-  console.log(`Example app listening on http://localhost:${port}`)
-})
-
+    console.log(`Example app listening on http://localhost:${port}`);
+});
