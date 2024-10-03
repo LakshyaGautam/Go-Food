d6f7bc2897b700e1fc9c9cee0ee968ce2abc3002
Lakshya Gautam <gautamlakshya021@gmail.com>
2024-10-03 12:47:55 +0530

    api updates

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -3,37 +3,7 @@ import { useNavigate, Link } from 'react-router-dom'
 import Navbar from '../components/Navbar';
 export default function Signup() {
   const [credentials, setCredentials] = useState({ name: "", email: "", password: "", geolocation: "" })
-  let [address, setAddress] = useState("");
   let navigate = useNavigate()
-
-  const handleClick = async (e) => {
-    e.preventDefault();
-    let navLocation = () => {
-      return new Promise((res, rej) => {
-        navigator.geolocation.getCurrentPosition(res, rej);
-      });
-    }
-    let latlong = await navLocation().then(res => {
-      let latitude = res.coords.latitude;
-      let longitude = res.coords.longitude;
-      return [latitude, longitude]
-    })
-    // console.log(latlong)
-    let [lat, long] = latlong
-    console.log(lat, long)
-    const response = await fetch("https://go-food-backend-ncy7.onrender.com/api/auth/getlocation", {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ latlong: { lat, long } })
-
-    });
-    const { location } = await response.json()
-    console.log(location);
-    setAddress(location);
-    setCredentials({ ...credentials, [e.target.name]: location })
-  }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -82,12 +52,7 @@ export default function Signup() {
             </div>
             <div className="m-3">
               <label htmlFor="address" className="form-label">Address</label>
-              <fieldset>
-                <input type="text" className="form-control" name='address' placeholder='"Click below for fetching address"' value={address} onChange={(e)=>setAddress(e.target.value)} aria-describedby="emailHelp" />
-              </fieldset>
-            </div>
-            <div className="m-3">
-              <button type="button" onClick={handleClick} name="geolocation" className=" btn btn-success">Click for current Location </button>
+                <input type="text" className="form-control" name='address' value={credentials.geolocation} onChange={onchange} aria-describedby="emailHelp" />
             </div>
             <div className="m-3">
               <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
