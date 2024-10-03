90188834cbd6d828ab057c12d4943d64645ee8da
Lakshya Gautam <gautamlakshya021@gmail.com>
2024-10-03 12:24:37 +0530

    frontend updates

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,87 +1,64 @@
-import React, { useEffect, useState } from 'react'
-import Card from '../components/Card'
-// import Carousel from '../components/Carousel'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
+import React, { useEffect, useState } from 'react';
+import Card from '../components/Card';
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+
 export default function Home() {
-  const [foodCat, setFoodCat] = useState([])
-  const [foodItems, setFoodItems] = useState([])
-  const [search, setSearch] = useState('')
-  const loadFoodItems = async () => {
-    let response = await fetch("https://go-food-backend-ncy7.onrender.com/api/auth/foodData", {
-      // credentials: 'include',
-      // Origin:"http://localhost:3000/login",
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      }
+    const [foodCat, setFoodCat] = useState([]);
+    const [foodItems, setFoodItems] = useState([]);
+    const [search, setSearch] = useState('');
 
-    });
-    response = await response.json()
-    // console.log(response[1][0].CategoryName)
-    setFoodItems(response[0])
-    setFoodCat(response[1])
-  }
+    const loadFoodItems = async () => {
+        let response = await fetch("https://go-food-backend-ncy7.onrender.com/api/auth/foodData", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        response = await response.json();
+        console.log(response); // Log the response
+        setFoodItems(response[0] || []); // Set to empty array if undefined
+        setFoodCat(response[1] || []); // Set to empty array if undefined
+    }
 
-  useEffect(() => {
-    loadFoodItems()
-  }, [])
+    useEffect(() => {
+        loadFoodItems();
+    }, []);
 
-  return (
-    <div >
-      <div>
-        <Navbar />
-      </div>
-      <div >
-
-        <div className='d-flex justify-content-center' style={{ height: "60vh", width: "100vw", position: "relative" }}>
-          <img src="/tandoori-paneer-tikka4.jpg" style={{ objectFit: "cover", filter: "brightness(50%)", height: "100%", width: "100%" }} alt="" />
-
-          <div style={{ zIndex: "9", position: "absolute", bottom: "10vh", width: "80%" }}>
-            <div className=" d-flex justify-content-center">  {/* justify-content-center, copy this <form> from navbar for search box */}
-              <input className="form-control me-2 w-75 bg-white text-dark" type="search" placeholder="Search in here..." aria-label="Search" value={search} onChange={(e) => { setSearch(e.target.value) }} />
-              <button className="btn text-white bg-danger" onClick={() => { setSearch('') }}>Clear</button>
-            </div>
-          </div>
-        </div>
-
-      </div>
-      <div className='container'> {/* boootstrap is mobile first */}
-        {
-          foodCat.length > 0
-            ? foodCat.map((data) => {
-              return (
-                // justify-content-center
-                <div key={data._id} className='row mb-3'>
-                  <div className='fs-3 m-3'>
-                    {data.CategoryName}
-                  </div>
-                  <hr id="hr-success" style={{ height: "4px", backgroundImage: "-webkit-linear-gradient(left,rgb(0, 255, 137),rgb(0, 0, 0))" }} />
-                  {foodItems.length > 0? foodItems.filter(
-                    (items) => (items.CategoryName === data.CategoryName) && (items.name.toLowerCase().includes(search.toLowerCase())))
-                    .map(filterItems => {
-                      return (
-                        <div key={filterItems._id} className='col-12 col-md-6 col-lg-3'>
-                          {console.log(filterItems.url)}
-                          <Card foodName={filterItems.name} item={filterItems} options={filterItems.options[0]} ImgSrc={filterItems.img} ></Card>
-                        </div>
-                      )
-                    }) : <div> No Such Data </div>}
+    return (
+        <div>
+            <Navbar />
+            <div className='d-flex justify-content-center' style={{ height: "60vh", width: "100vw", position: "relative" }}>
+                <img src="/tandoori-paneer-tikka4.jpg" style={{ objectFit: "cover", filter: "brightness(50%)", height: "100%", width: "100%" }} alt="" />
+                <div style={{ zIndex: "9", position: "absolute", bottom: "10vh", width: "80%" }}>
+                    <div className="d-flex justify-content-center">
+                        <input className="form-control me-2 w-75 bg-white text-dark" type="search" placeholder="Search in here..." aria-label="Search" value={search} onChange={(e) => { setSearch(e.target.value) }} />
+                        <button className="btn text-white bg-danger" onClick={() => { setSearch('') }}>Clear</button>
+                    </div>
                 </div>
-              )
-            })
-            : ""}
-      </div>
-      <Footer />
-    </div>
-
-
-
-
-
-
-
-
-
-  )
+            </div>
+            <div className='container'>
+                {Array.isArray(foodCat) && foodCat.length > 0 ? foodCat.map((data) => {
+                    return (
+                        <div key={data._id} className='row mb-3'>
+                            <div className='fs-3 m-3'>
+                                {data.CategoryName}
+                            </div>
+                            <hr id="hr-success" style={{ height: "4px", backgroundImage: "-webkit-linear-gradient(left,rgb(0, 255, 137),rgb(0, 0, 0))" }} />
+                            {Array.isArray(foodItems) && foodItems.length > 0 ? foodItems.filter(
+                                (items) => (items.CategoryName === data.CategoryName) && (items.name.toLowerCase().includes(search.toLowerCase()))
+                            ).map(filterItems => {
+                                return (
+                                    <div key={filterItems._id} className='col-12 col-md-6 col-lg-3'>
+                                        <Card foodName={filterItems.name} item={filterItems} options={filterItems.options[0]} ImgSrc={filterItems.img}></Card>
+                                    </div>
+                                )
+                            }) : <div>No Such Data</div>}
+                        </div>
+                    )
+                }) : <div>Loading categories...</div>}
+            </div>
+            <Footer />
+        </div>
+    );
 }
